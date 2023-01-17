6ba99ae02dfc2f7409f7c3bfeabee4c015d7355d
Straga07 <Straga07@yandex.ru>
2023-01-17 17:03:39 +0300

    feat: test of instanceof

--- Practicum/src/Main.java
+++ Practicum/src/Main.java
@@ -1,74 +1,144 @@
+
+
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Scanner;
+
+class Calculator {
+
+    private Calculator() {}
+
+    public static double calculate(List<MediaItem> mediaItems) {
+        // Напишите реализацию метода, который будет возвращать общее количество дней,
+        // потраченных на просмотр фильмов и сериалов
+        double count=0;
+        //boolean isPassport = mediaItems instanceof Movie;
+
+        for (MediaItem mediaItem : mediaItems) {
+            if (mediaItem instanceof Movie) {
+                count += mediaItem.getRuntime();
+            } else if (mediaItem instanceof Series) {
+                //System.out.println("111111111111111111 "+mediaItems.get(key).getRuntime());
+                count += mediaItem.getRuntime()* ((Series) mediaItem).getSeriesCount();
+
+            }
+        }
+        return count/(60*24);
+    }
+
+}
+interface MediaItem {
+
+    // Длительность фильма или сериала в минутах
+    public int getRuntime();
+
+    public String getTitle();
+
+}
+ class Movie implements MediaItem {
+
+    private final String title;
+    private final int runtime;
+
+    public Movie(String title, int runtime) {
+        this.title = title;
+        this.runtime = runtime;
+    }
+
+    @Override
+    public int getRuntime() {
+        return runtime;
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+}
+ class Series implements MediaItem {
+
+    private final String title;
+    private final int averageRuntime;
+    private final int seriesCount;
+
+    public Series(String title, int averageRuntime, int seriesCount) {
+        this.title = title;
+        this.averageRuntime = averageRuntime;
+        this.seriesCount = seriesCount;
+    }
+
+    @Override
+    public int getRuntime() {
+        return averageRuntime;
+    }
+
+    @Override
+    public String getTitle() {
+        return title;
+    }
+
+    public int getSeriesCount() {
+        return seriesCount;
+    }
+}
 
 public class Main {
 
     public static void main(String[] args) {
-        String name = "Катя";
+        List<MediaItem> mediaItems = new ArrayList<>();
 
-        System.out.println("Что " + name + " делает как работник:");
-        Employee employee = new SoftwareDeveloper(name);
-        employee.work();
-        System.out.println("Получает зарплату за месяц: " + employee.getSalary() + " р.");
-        // Таких методов в интерфейсе Employee нет - произойдет ошибка
-        // employee.eat();
-        // employee.doDebug();
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("\nЧто " + name + " делает как человек:");
-        Person person = new SoftwareDeveloper(name);
-        person.eat();
-        person.sleep();
-        // Таких методов в классе Person нет - произойдет ошибка
-        // person.work();
-        // person.doDebug();
+        while (true) {
+            printMenu();
+            // Считайте команду, введенную пользователем
+            int command = scanner.nextInt();
 
-        System.out.println("\nЧто " + name + " делает как программист:");
-        SoftwareDeveloper softwareDeveloper = new SoftwareDeveloper(name);
-        softwareDeveloper.doDebug();
-        softwareDeveloper.think();
+            if (command == 1) {
+                System.out.println("Введите название фильма:");
+                String title = scanner.next();
+                System.out.println("Введите длительность фильма в минутах:");
+                int runtime = scanner.nextInt();
+
+                // На основе введенных пользователем значений создайте объект класса Movie
+                Movie movie = new Movie(title,runtime);
+                mediaItems.add(movie);
+            } else if (command == 2) {
+                System.out.println("Введите название сериала:");
+                String title = scanner.next();
+                System.out.println("Введите количество серий:");
+                int seriesCount = scanner.nextInt();
+                System.out.println("Введите среднюю длительность серии в минутах");
+                int runtime = scanner.nextInt();
+
+                // Создайте сериал и добавьте его в список просмотренных
+                Series series = new Series(title,runtime,seriesCount);
+                mediaItems.add(series);
+            } else if (command == 0) {
+                printMediaItemsList(mediaItems);
+
+                double totalRuntime = Calculator.calculate(mediaItems);
+                System.out.println("Всего вы потратили на просмотр фильмов и сериалов, в днях: " + totalRuntime);
+                break;
+            }
+        }
     }
+
+    public static void printMenu() {
+        System.out.println("1 - Добавить фильм");
+        System.out.println("2 - Добавить сериал");
+        System.out.println("0 - Посчитать суммарное время и выйти");
+    }
+
+
+    public static void printMediaItemsList(List<MediaItem> mediaItems) {
+        System.out.println("Вы посмотрели фильмов и сериалов: " + mediaItems.size());
+
+        for (MediaItem films: mediaItems) {
+            System.out.println(films.getTitle());
+        }
+
+    }
+
 }
-interface Employee {
-    void work();
 
-    double getSalary();
-    //void eat();
-}
-class Person {
-    private final String name;
-
-    public Person(String name) {
-        this.name = name;
-    }
-
-    public void eat() {
-        System.out.println("Обедает в кафе.");
-    }
-
-    public void sleep() {
-        System.out.println("Крепко спит всю ночь.");
-    }
-}
-class SoftwareDeveloper extends Person implements Employee {
-
-    public SoftwareDeveloper(String name) {
-        super(name);
-    }
-
-    @Override
-    public void work() {
-        System.out.println("Выполняет свою работу - пишет код весь день.");
-    }
-
-    @Override
-    public double getSalary() {
-        return 100500.0;
-    }
-
-    public void doDebug() {
-        System.out.println("Ищет ошибки в коде.");
-    }
-
-    public void think() {
-        System.out.println("Обдумывает решение задачи.");
-    }
-}
